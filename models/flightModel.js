1a1c23134052e1c1e860f40d10dd45f5430ab735
romlopez1 <romibernard68@gmail.com>
2021-09-27 13:29:05 -0500

    schema de vuelos

--- models/flightModel.js
+++ models/flightModel.js
@@ -4,5 +4,38 @@ const flightSchema = mongoose.Schema({
     date: {
         type: Number,
         required: [true, "Por favor ingresa una fecha"],
+    },
+    from: {
+        type: Number,
+        required: true
+    },
+    to: {
+        type: Number,
+        required: true 
+    },
+    kind: String, //local, ruta
+    plate: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    model: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    time: {
+        type: Number,
+        required: true
+    },
+    plus: { //vfr, ifr, nocturna
+        type: String,
+        required: true
     }
 })
+
+// modelo
+const Flight = mongoose.model("Flight", flightSchema)
+
+// exportación
+module.exports = Flight
